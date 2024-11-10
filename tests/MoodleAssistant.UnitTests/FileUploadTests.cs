ed236bf2b29634b5ebb0f5ea7214a79ffd3e1177
S0Ale <78408616+S0Ale@users.noreply.github.com>
2024-11-10 13:08:16 +0100

    Switched From xunit to NUnit

---
+++ tests/MoodleAssistant.UnitTests/FileUploadTests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+
+namespace MoodleAssistant.UnitTests;
+
+[TestFixture]
+internal class FileUploadTests {
+    [SetUp]
+    public void Setup() {
+    }
+
+    [Test]
+    public void Test1() {
+        Assert.Pass();
+    }
+
+    [TearDown]
+    public void TearDown() {
+    }
+}
