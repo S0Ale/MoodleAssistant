ba591a0df3ee1e32fc78fd2a9b5ae4dd6b676761
S0Ale <78408616+S0Ale@users.noreply.github.com>
2024-11-23 17:14:46 +0100

    Added state

--- MoodleAssistant/Program.cs
+++ MoodleAssistant/Program.cs
@@ -1,10 +1,13 @@
 using MoodleAssistant.Components;
+using MoodleAssistant.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+
+builder.Services.AddScoped<ReplicatorState>();
 
 var app = builder.Build();
 
---
+++ MoodleAssistant/States/ReplicatorState.cs
@@ -0,0 +1,8 @@
+﻿using System.Xml;
+
+namespace MoodleAssistant.Services;
+
+public class ReplicatorState{
+    public XmlDocument TemplateQuestion{ get; set; }
+    public IEnumerable<string[]> CsvAsList{ get; set; }
+}
