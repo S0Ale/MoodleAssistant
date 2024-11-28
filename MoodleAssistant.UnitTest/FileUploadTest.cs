f96932c3ba767a23bf7aa6ba96f23658f99c51b1
S0Ale <78408616+S0Ale@users.noreply.github.com>
2024-11-28 14:04:49 +0100

    Added Unit test

---
+++ MoodleAssistant.UnitTest/FileUploadTest.cs
@@ -0,0 +1,11 @@
+namespace MoodleAssistant.UnitTest;
+
+public class FileUploadTest{
+    [SetUp]
+    public void Setup(){ }
+
+    [Test]
+    public void Test1(){
+        Assert.Pass();
+    }
+}
