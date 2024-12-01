0ff28697d8247987d344fa4740a30cc26610976d
S0Ale <78408616+S0Ale@users.noreply.github.com>
2024-12-01 17:10:29 +0100

    Working Test setup

--- MoodleAssistant.UnitTest/FileUploadTest.cs
+++ MoodleAssistant.UnitTest/FileUploadTest.cs
@@ -1,4 +1,8 @@
 using Bunit;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using MoodleAssistant.Services;
+using Moq;
 
 namespace MoodleAssistant.UnitTest;
 
@@ -8,6 +12,15 @@ public class FileUploadTest{
     [SetUp]
     public void Setup(){
         Ctx = new BunitContext();
+        var env = new Mock<IWebHostEnvironment>(); // faking IWebHostEnvironment
+        env.Setup(e => e.WebRootPath).Returns(@"C:\Users\user\Desktop\UNI\MoodleAssistant\MoodleAssistant\wwwroot");
+        Ctx.Services.AddScoped<IWebHostEnvironment>(e => env.Object);
+        
+        Ctx.JSInterop.Mode = JSRuntimeMode.Loose;
+        //Ctx.JSInterop.SetupModule(@"C:\Users\user\Desktop\UNI\MoodleAssistant\MoodleAssistant\Components\Upload\FileDropInput.razor.js");
+        
+        Ctx.Services.AddScoped<IBrowserFileService, FileService>();
+        Ctx.Services.AddScoped<ReplicatorState>();
     }
 
     [TearDown]
