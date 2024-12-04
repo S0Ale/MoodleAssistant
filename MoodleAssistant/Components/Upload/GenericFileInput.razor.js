c16822f1c3f5c2ff6063627312c1de1e851e6a33
S0Ale <78408616+S0Ale@users.noreply.github.com>
2024-12-04 14:50:30 +0100

    Added generic file input with drop support

---
+++ MoodleAssistant/Components/Upload/GenericFileInput.razor.js
@@ -0,0 +1,14 @@
+﻿export function initComponent(id) {
+    let item = query(`#${id}`);
+    const input = queryChilds(item, 'input');
+    const dropZone = queryChilds(item, '.drop-zone');
+
+    dropZone.addEventListener('dragover', function (e) {
+        e.preventDefault();
+    });
+    dropZone.addEventListener('drop', function (e) {
+        e.preventDefault();
+        input.files = e.dataTransfer.files;
+        input.dispatchEvent(new Event('change'));
+    });
+}
