0b8e5f63ea5632bf17eb9534e5fc28eca36dcc5c
S0Ale <78408616+S0Ale@users.noreply.github.com>
2024-12-04 18:38:47 +0100

    Generic drag @ drop cumulative file input

---
+++ MoodleAssistant/Components/Upload/DropFileInput.razor.js
@@ -0,0 +1,19 @@
+﻿export function initComponent(id) {
+    let item = query(id);
+    let dropZones = queryChilds(item, '.drop-zone');
+    if(!Array.isArray(dropZones)) dropZones = [dropZones];
+    console.log(dropZones);
+
+    dropZones.forEach(dropZone => {
+        const input = queryChilds(dropZone, 'input');
+        
+        dropZone.addEventListener('dragover', function (e) {
+            e.preventDefault();
+        });
+        dropZone.addEventListener('drop', function (e) {
+            e.preventDefault();
+            input.files = e.dataTransfer.files;
+            input.dispatchEvent(new Event('change'));
+        });
+    });
+}
