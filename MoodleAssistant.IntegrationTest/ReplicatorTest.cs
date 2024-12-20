1b10769a179c9c34a51f8b9133b9ac8109acb15c
S0Ale <78408616+S0Ale@users.noreply.github.com>
2024-12-20 16:08:55 +0100

    Working Integration tests

--- MoodleAssistant.IntegrationTest/ReplicatorTest.cs
+++ MoodleAssistant.IntegrationTest/ReplicatorTest.cs
@@ -6,70 +6,58 @@ using NUnit.Framework;
 
 namespace MoodleAssistant.IntegrationTest;
 
+[Parallelizable(ParallelScope.Self)]
 [TestFixture]
 public class ReplicatorTest : PageTest{
-    
-    private string _url = "https://localhost:7085/replicator";
-    //private string _main
-
+    private const string Url = "https://localhost:7085/replicator";
+ 
     [TestCase("MoodleQuestionOk.xml", "MoodleQuestionOk.csv")]
     [TestCase("multichoice.xml", "multichoice.csv")]
-    [TestCase("Multichoice1.xml", "Multichoice1.csv")][Ignore("Multiple parameter occurrences are not supported")]
+    [TestCase("Multichoice1.xml", "Multichoice1.csv")]
     [TestCase("trueFalse.xml", "trueFalse.csv")]
+    [TestCase("shortAnswer.xml", "shortAnswer.csv")]
     public async Task UploadFiles_BasicQuestion(string xmlName, string csvName){
-        await Page.GotoAsync(_url);
+        await Page.GotoAsync(Url);
         var xml = Page.GetByTestId("xml-input");
         var csv = Page.GetByTestId("csv-input");
+        await Task.Delay(1000);
         
         await xml.SetInputFilesAsync(TestService.GetPath(xmlName));
         await csv.SetInputFilesAsync(TestService.GetPath(csvName));
         
         await Page.GetByTestId("main-submit").ClickAsync();
-        // Wait for the download button to appear, with a 10-second timeout
-        await TestService.Screenshot(Page, $"upload-{Path.GetFileNameWithoutExtension(xmlName)}");
-        /*
-        try{
-            downloadBtn = await Page.WaitForSelectorAsync("[data-testid=download]",
-                new PageWaitForSelectorOptions{ Timeout = 10000 });
-        }catch{
-            await TestService.Screenshot(Page, $"error-{Path.GetFileNameWithoutExtension(xmlName)}");
-            Assert.Fail("Download button did not appear");
-        }
-        */
-
-        await TestService.Screenshot(Page, $"success-{Path.GetFileNameWithoutExtension(xmlName)}");
-        await Expect(Page.GetByTestId("download")).ToBeVisibleAsync(new(){Timeout = 10000});
+        await Expect(Page.GetByTestId("download")).ToBeVisibleAsync();
     }
 
-    [Test][Ignore("This test is not working")]
+    [Test][Category("File")]
     public async Task UploadFiles_QuestionWithImage(){
+        await Page.GotoAsync(Url);
         var xml = Page.GetByTestId("xml-input");
         var csv = Page.GetByTestId("csv-input");
+        await Task.Delay(1000);
         
         await xml.SetInputFilesAsync(TestService.GetPath("MoodleOkWithImage.xml"));
         await csv.SetInputFilesAsync(TestService.GetPath("MoodleOkWithImage.csv"));
         
         await Page.GetByTestId("main-submit").ClickAsync();
-    }
-
-    [Test]
-    public async Task Screen_Test(){
-        await Page.GotoAsync(_url);
-        var xml = Page.GetByTestId("xml-input");
-        var csv = Page.GetByTestId("csv-input");
-
-        await TestService.Screenshot(Page, "start-file-upload");
+        await Task.Delay(2000);
+        var fileSection = Page.GetByTestId("file-form");
         
-        await xml.SetInputFilesAsync(TestService.GetPath("MoodleOkWithImage.xml"));
-        await csv.SetInputFilesAsync(TestService.GetPath("MoodleOkWithImage.csv"));
+        await Expect(fileSection).ToBeVisibleAsync();
         
-        await Page.GetByTestId("main-submit").ClickAsync();
-        var reset = await Page.WaitForSelectorAsync("[data-testid=reset]",
-            new PageWaitForSelectorOptions{ Timeout = 10000 });
+        var inputs = await fileSection.GetByTestId("IMAGE-number-input").AllAsync();
+        await Task.Delay(1000);
         
-        await TestService.Screenshot(Page, "after-file-upload", true);
-
-        Assert.Pass();
+        // Set the files for the inputs (one input cannot have multiple files)
+        var files = new[]{TestService.GetPath("Image1.png"), TestService.GetPath("Image2.png"), TestService.GetPath("Image3.png")};
+        var i = 0;
+        foreach (var input in inputs){
+            await input.SetInputFilesAsync(files[i]);
+            i++;
+        }
+            
+        await Page.GetByTestId("snd-submit").ClickAsync();
+        await Expect(Page.GetByTestId("download")).ToBeVisibleAsync();
     }
 
 }
