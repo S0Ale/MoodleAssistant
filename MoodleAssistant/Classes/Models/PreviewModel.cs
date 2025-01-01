4b40c590556fdb07351a8d08a343aeffeb9beee1
S0Ale <78408616+S0Ale@users.noreply.github.com>
2025-01-01 19:34:58 +0100

    Resolved memory issue

--- MoodleAssistant/Classes/Models/PreviewModel.cs
+++ MoodleAssistant/Classes/Models/PreviewModel.cs
@@ -4,8 +4,6 @@ namespace MoodleAssistant.Classes.Models;
 
 public class PreviewModel{
     public List<PreviewItem> Items{ get; } = [];
-    //public IEnumerable<string> QuestPreviews; // more than 1 question is not allowed
-
 
     public PreviewModel(XmlDocument question, int answers){
         GenerateItems(question, answers);
@@ -18,13 +16,14 @@ public class PreviewModel{
         using var e1 = e as IDisposable;
 
         for (var i = 0; i < questions.Count; i++){
-            var item = new PreviewItem(questions[i]?.InnerText ?? string.Empty);
+            var question = questions[i];
+            var item = new PreviewItem(question?.SelectSingleNode("text")?.InnerText ?? string.Empty);
 
             var answerStrings = new string[answerCount];
             for (var j = 0; j < answerCount; j++) {
                 e.MoveNext();
                 var node = e.Current as XmlNode;
-                answerStrings[j] = node?.InnerText ?? string.Empty;
+                answerStrings[j] = node?.SelectSingleNode("text")?.InnerText ?? string.Empty;
             }
 
             item.Answers = answerStrings;
