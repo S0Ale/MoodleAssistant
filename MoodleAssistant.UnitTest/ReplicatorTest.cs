116c1d8994404b8c37800b7ac8b9ddcbbe7ff973
S0Ale <78408616+S0Ale@users.noreply.github.com>
2025-01-02 12:11:59 +0100

    Test fixes

--- MoodleAssistant.UnitTest/ReplicatorTest.cs
+++ MoodleAssistant.UnitTest/ReplicatorTest.cs
@@ -7,29 +7,35 @@ using MoodleAssistant.Services;
 namespace MoodleAssistant.UnitTest;
 
 internal class ReplicatorTest : FileUploadTest{
+    
+    private IRenderedComponent<Replicator> _page;
 
     [SetUp]
-    public new void Setup(){ }
+    public new void Setup(){
+        base.Setup();
+        _page = Ctx.Render<Replicator>();
+    }
 
     [Test]
     public void Upload_MergedIsNotNull(){
-        var page = Ctx.Render<Replicator>();
-        var inputs = page.FindComponents<InputFile>();
+        var inputs = _page.FindComponents<InputFile>();
         inputs[0].UploadFiles(TestService.Create("MoodleQuestionOk.xml", System.Net.Mime.MediaTypeNames.Text.Xml));
         inputs[1].UploadFiles(TestService.Create("MoodleQuestionOk.csv", System.Net.Mime.MediaTypeNames.Text.Csv));
-        page.Find("button[type=submit]").Click();
-        page.WaitForState(() => page.Instance.IsUploading == false);
+        _page.Find("button[type=submit]").Click();
+        _page.WaitForState(() => _page.Instance.IsUploading == false);
         
         var state = Ctx.Services.GetService<ReplicatorState>();
         
         Assert.Multiple(() => {
-            Assert.That(page.Instance.Error, Is.EqualTo(Error.NoErrors));
-            Assert.That(page.Instance.SuccessUpload, Is.True);
+            Assert.That(_page.Instance.Error, Is.EqualTo(Error.NoErrors));
+            Assert.That(_page.Instance.SuccessUpload, Is.True);
             Assert.That(state?.Merged, Is.Not.Null);
         });
     }
 
     [TearDown]
-    public new void TearDown(){ }
+    public new void TearDown(){
+        _page.Dispose();
+    }
 
 }
