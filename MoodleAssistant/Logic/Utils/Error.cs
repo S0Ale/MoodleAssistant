225062a6e5e92807771a85097f4fd6a49b44aa16
S0Ale <78408616+S0Ale@users.noreply.github.com>
2025-01-06 19:24:50 +0100

    Error refactoring

--- MoodleAssistant/Logic/Utils/Error.cs
+++ MoodleAssistant/Logic/Utils/Error.cs
@@ -1,65 +1,14 @@
 ﻿namespace MoodleAssistant.Logic.Utils;
 
-public static class ErrorMessage
-{
-    // Validation errors
-    //common
-    private const string NoErrors = "";
-    private const string NoFiles = "Missing files. Please upload your files.";
-    private const string NullFile = "Upload a file.";
-    private const string EmptyFile = "File cannot be empty.";
-    private const string Unexpected = "An unexpected error occurred. Try reloading the page.";
-
-    //xml errors
-    private const string NonXmlFile = "File type must be XML.";
-    private const string MalFormatted = "XML file is bad formed.";
-    private const string ZeroOrMoreQuestions = "XML file must contains only one question.";
-    private const string ZeroAnswers = "XML quiz must contains answers.";
-
-    //csv errors
-    private const string NonCsvFile = "File type must be CSV.";
-    private const string CsvInvalidHeader = "Please enter a valid CSV file. Use \",\" delimiter. One or more csv header missing.";
-    private const string CsvBadFormed = "Please enter a valid CSV file. Check if there are missing fields or if each row contains the same number of columns.";
-    
-    // file errors
-    private const string NoValidFile = "Invalid file found.";
-    
-    // Merge errors
-    private const string FileMismatch = "File names need to be equal to the names inside the CSV file.";
-    
-    public static string GetErrorMessage(Error error)
-    {
-        return error switch
-        {
-            Error.NoErrors => NoErrors,
-            Error.NoFiles => NoFiles,
-            Error.NullFile => NullFile,
-            Error.Unexpected => Unexpected,
-            
-            Error.NonXmlFile => NonXmlFile,
-            Error.EmptyFile => EmptyFile,
-            Error.XmlBadFormed => MalFormatted,
-            Error.ZeroOrMoreQuestions => ZeroOrMoreQuestions,
-            Error.ZeroAnswers => ZeroAnswers,
-            Error.NonCsvFile => NonCsvFile,
-            Error.CsvInvalidHeader => CsvInvalidHeader,
-            Error.CsvBadFormed => CsvBadFormed,
-            Error.NoValidFile => NoValidFile,
-            
-            Error.FileMismatch => FileMismatch,
-            
-            _ => NoErrors
-        };
-    }
-}
-
-public enum Error
-{
+/// <summary>
+/// Represents an error that occurred in the replication process.
+/// </summary>
+public enum Error{
     NoErrors,
     NoFiles,
     NullFile,
     Unexpected,
-    
+        
     NonXmlFile,
     EmptyFile,
     XmlBadFormed,
@@ -68,8 +17,8 @@ public enum Error
     NonCsvFile,
     CsvInvalidHeader,
     CsvBadFormed,
-    
+        
     NoValidFile,
-    
+        
     FileMismatch
 }
---
+++ MoodleAssistant/Logic/Utils/ErrorMessage.cs
@@ -0,0 +1,62 @@
+﻿namespace MoodleAssistant.Logic.Utils;
+
+/// <summary>
+/// Contains error messages for the application.
+/// </summary>
+public static class ErrorMessage
+{
+    // Validation errors
+    //common
+    private const string NoErrors = "";
+    private const string NoFiles = "Missing files. Please upload your files.";
+    private const string NullFile = "Upload a file.";
+    private const string EmptyFile = "File cannot be empty.";
+    private const string Unexpected = "An unexpected error occurred. Try reloading the page.";
+
+    //xml errors
+    private const string NonXmlFile = "File type must be XML.";
+    private const string MalFormatted = "XML file is bad formed.";
+    private const string ZeroOrMoreQuestions = "XML file must contains only one question.";
+    private const string ZeroAnswers = "XML quiz must contains answers.";
+
+    //csv errors
+    private const string NonCsvFile = "File type must be CSV.";
+    private const string CsvInvalidHeader = "Please enter a valid CSV file. Use \",\" delimiter. One or more csv header missing.";
+    private const string CsvBadFormed = "Please enter a valid CSV file. Check if there are missing fields or if each row contains the same number of columns.";
+    
+    // file errors
+    private const string NoValidFile = "Invalid file found.";
+    
+    // Merge errors
+    private const string FileMismatch = "File names need to be equal to the names inside the CSV file.";
+
+    /// <summary>
+    /// Returns the error message based on the error code.
+    /// </summary>
+    /// <param name="error">The <see cref="Error"/> that occurred.</param>
+    /// <returns>The string matching with the specified <see cref="Error"/>.</returns>
+    public static string GetErrorMessage(Error error)
+    {
+        return error switch
+        {
+            Error.NoErrors => NoErrors,
+            Error.NoFiles => NoFiles,
+            Error.NullFile => NullFile,
+            Error.Unexpected => Unexpected,
+            
+            Error.NonXmlFile => NonXmlFile,
+            Error.EmptyFile => EmptyFile,
+            Error.XmlBadFormed => MalFormatted,
+            Error.ZeroOrMoreQuestions => ZeroOrMoreQuestions,
+            Error.ZeroAnswers => ZeroAnswers,
+            Error.NonCsvFile => NonCsvFile,
+            Error.CsvInvalidHeader => CsvInvalidHeader,
+            Error.CsvBadFormed => CsvBadFormed,
+            Error.NoValidFile => NoValidFile,
+            
+            Error.FileMismatch => FileMismatch,
+            
+            _ => NoErrors
+        };
+    }
+}
