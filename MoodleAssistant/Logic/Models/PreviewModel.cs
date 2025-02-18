0aafbebb2f221e76151b522a4eba9683a073953a
S0Ale <78408616+S0Ale@users.noreply.github.com>
2025-02-18 16:08:16 +0100

    Preview fixes

--- MoodleAssistant/Logic/Models/PreviewModel.cs
+++ MoodleAssistant/Logic/Models/PreviewModel.cs
@@ -15,34 +15,39 @@ public class PreviewModel{
     /// Initializes a new instance of the <see cref="PreviewModel"/> class.
     /// </summary>
     /// <param name="question">The current <see cref="XmlDocument"/>.</param>
-    /// <param name="answers">The current <see cref="XmlDocument"/>'s answer count.</param>
     public PreviewModel(XmlDocument question){
-        GenerateItems(question, question.GetElementsByTagName("answer").Count);
+        GenerateItems(question);
     }
 
     /// <summary>
     /// Generates all the preview items of a question.
     /// </summary>
     /// <param name="doc">The current <see cref="XmlDocument"/>.</param>
-    /// <param name="answerCount">The current <see cref="XmlDocument"/>'s answer count.</param>
-    private void GenerateItems(XmlDocument doc, int answerCount) {
-        var questions = doc.GetElementsByTagName("questiontext");
-        var answers = doc.GetElementsByTagName("answer");
-        var e = answers.GetEnumerator();
-        using var e1 = e as IDisposable;
+    private void GenerateItems(XmlDocument doc) {
+        var questions = doc.GetElementsByTagName("question");
+        //var answers = doc.GetElementsByTagName("answer");
+        //var e = answers.GetEnumerator();
+        //using var e1 = e as IDisposable;
 
         for (var i = 0; i < questions.Count; i++){
             var question = questions[i];
-            var item = new PreviewItem(question?.SelectSingleNode("text")?.InnerText ?? string.Empty);
+            var item = new PreviewItem(question?.SelectSingleNode("questiontext")?.SelectSingleNode("text")?.InnerText ?? string.Empty);
 
-            var answerStrings = new string[answerCount];
-            for (var j = 0; j < answerCount; j++) {
-                e.MoveNext();
-                var node = e.Current as XmlNode;
-                answerStrings[j] = node?.SelectSingleNode("text")?.InnerText ?? string.Empty;
+            var answers = question?.SelectNodes("answer");
+            if (answers != null){
+                var e = answers.GetEnumerator();
+                using var e1 = e as IDisposable;
+            
+                var answerStrings = new string[answers.Count];
+                for (var j = 0; j < answers.Count; j++) {
+                    e.MoveNext();
+                    var node = e.Current as XmlNode;
+                    answerStrings[j] = node?.SelectSingleNode("text")?.InnerText ?? string.Empty;
+                }
+                item.Answers = answerStrings;
             }
 
-            item.Answers = answerStrings;
+
             Items.Add(item);
         }
     }
