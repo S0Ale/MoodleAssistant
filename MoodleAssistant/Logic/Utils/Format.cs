7c78253de7f0e03e2720d41f4d964ca52fa3d11c
S0Ale <78408616+S0Ale@users.noreply.github.com>
2025-02-19 12:59:05 +0100

    New format enums

---
+++ MoodleAssistant/Logic/Utils/Format.cs
@@ -0,0 +1,11 @@
+﻿namespace MoodleAssistant.Logic.Utils;
+
+/// <summary>
+/// Represents the format of the template question file.
+/// </summary>
+public enum Format{
+    /// <summary>
+    /// The format is XML.
+    /// </summary>
+    Xml
+}
---
+++ MoodleAssistant/Logic/Utils/FormatExtension.cs
@@ -0,0 +1,24 @@
+﻿namespace MoodleAssistant.Logic.Utils;
+
+/// <summary>
+/// Contains extension methods for file formats.
+/// </summary>
+public class FormatExtension{
+    /// <summary>
+    /// The extension for XML files.
+    /// </summary>
+    private const string Xml = ".xml";
+    
+    /// <summary>
+    /// Returns the extension of the specified format.
+    /// </summary>
+    /// <param name="format">The specified <see cref="Format"/>.</param>
+    /// <returns>The extension of the specified <see cref="Format"/>.</returns>
+    /// <exception cref="ArgumentOutOfRangeException">Thrown when the specified format is not present.</exception>
+    public static string GetExtension(Format format){
+        return format switch{
+            Format.Xml => Xml,
+            _ => throw new ArgumentOutOfRangeException(nameof(format), format, null)
+        };
+    }
+}
