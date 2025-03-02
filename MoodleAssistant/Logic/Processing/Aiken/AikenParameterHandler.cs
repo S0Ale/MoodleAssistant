e595c4b072e9a83eccb107762117e9287f39732a
S0Ale <78408616+S0Ale@users.noreply.github.com>
2025-03-02 23:36:27 +0100

    Added Aiken arameter handler

---
+++ MoodleAssistant/Logic/Processing/Aiken/AikenParameterHandler.cs
@@ -0,0 +1,14 @@
+﻿using AikenDoc;
+using MoodleAssistant.Logic.Parse;
+
+namespace MoodleAssistant.Logic.Processing.Aiken;
+
+/// <summary>
+/// Manager for the parameters of the Aiken document.
+/// </summary>
+public class AikenParameterHandler : ParameterHandler{
+    
+    public AikenParameterHandler(AikenDocument doc, int csvRows) : base(csvRows){
+        //Param = new ParameterParser(doc.OuterXml).Match() as List<Parameter>;
+    }
+}
