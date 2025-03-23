a6f0056106e641baf591dd387628dea154e0859e
S0Ale <78408616+S0Ale@users.noreply.github.com>
2025-03-23 23:03:51 +0100

    tutorial aiken section

---
+++ MoodleAssistant/wwwroot/js/tutorial.js
@@ -0,0 +1,17 @@
+﻿let index = 0;
+const select = document.querySelector('#tutorial-format');
+console.log(select);
+select.addEventListener('change', (event) => {
+    //reset all sections
+    let sections = document.querySelectorAll('.format-section');
+    sections.forEach(section => {
+        section.classList.add('hidden');
+    });
+
+    //parse vlue to int
+    index = parseInt(event.target.value);
+    
+    let toDisplay = document.querySelector(`.format-${index}`);
+    toDisplay.classList.remove('hidden');
+    console.log(toDisplay);
+});
